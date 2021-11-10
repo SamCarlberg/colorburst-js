2d49e7477649399df70cbaa7487e16f6710b57d4
Sam Carlberg <sam@slfc.dev>
2021-11-09 19:44:25 -0500

    Fix tab indentations

--- js/random.js
+++ js/random.js
@@ -9,25 +9,25 @@
  * random2.nextRandom(); // => 0.07329497812315822
  */
 class Random {
-	constructor(seed) {
-		this.seed = seed;
-		this.previous = seed;
-	}
+  constructor(seed) {
+    this.seed = seed;
+    this.previous = seed;
+  }
 
-	/**
-	 * Gets the next random number in the sequence.
-	 * Random numbers are returned as floats in the range [0, 1).
-	 */
-	nextRandom() {
-		const rand = this.mulberry32(this.previous);
-		this.previous = rand;
-		return rand / 4294967296; // map to [0, 1) range
-	}
+  /**
+   * Gets the next random number in the sequence.
+   * Random numbers are returned as floats in the range [0, 1).
+   */
+  nextRandom() {
+    const rand = this.mulberry32(this.previous);
+    this.previous = rand;
+    return rand / 4294967296; // map to [0, 1) range
+  }
 
-	mulberry32(a) {
-      var t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0);
-	}
+  mulberry32(a) {
+    var t = a += 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0);
+  }
 }
